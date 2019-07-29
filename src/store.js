5c631b0cc8d33dcc3386c50150b5c78ccc4f37ec
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-29 14:18:51 -0600

    finished step 5

--- src/store.js
+++ src/store.js
@@ -3,12 +3,16 @@ import {createStore} from 'redux'
 //INITIAL STATE
 const initialState = {
     recipeName: '',
-    recipeCategory: ''
+    recipeCategory: '',
+    authorFirstName: '',
+    authorLastName: ''
 }
 
 //ACTION CONSTS
 export const UPDATE_RECIPE_NAME = 'EXPORT_RECIPE_NAME'
 export const UPDATE_RECIPE_CATEG = 'EXPORT_RECIPE_CATEG'
+export const UPDATE_FIRST_NAME = 'UPDATE_FIRST_NAME'
+export const UPDATE_LAST_NAME = 'UPDATE_LAST_NAME'
 
 //REDUCER
 function reducer(state = initialState, action) {
@@ -17,6 +21,10 @@ function reducer(state = initialState, action) {
             return {...state, recipeName: action.payload}
         case UPDATE_RECIPE_CATEG:
             return {...state, recipeCategory: action.payload}
+        case UPDATE_FIRST_NAME:
+            return {...state, authorFirstName: action.payload}
+        case UPDATE_LAST_NAME:
+            return {...state, authorLastName: action.payload}
         default: return state
     }
 }
