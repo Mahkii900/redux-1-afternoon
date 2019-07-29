5e2842c52a25dd60ed23f8187b5033050f64c726
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-29 14:02:55 -0600

    first commit

--- src/store.js
+++ src/store.js
@@ -1,1 +1,18 @@
+import {createStore} from 'redux'
 
+//INITIAL STATE
+const initialState = {
+
+}
+
+//ACTION CONSTS
+
+//REDUCER
+const reducer(state = initialState, action) {
+    switch(action.type) {
+        default: return state
+    }
+}
+
+//EXPORTER
+export default createStore(reducer)
